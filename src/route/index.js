756c81b9b460e6628060fdd78d5d2be172491cd5
Evgen Gonshar <school.re17@gmail.com>
2023-08-30 13:02:37 +0300

    upd

--- src/route/index.js
+++ src/route/index.js
@@ -52,7 +52,7 @@ router.get('/', function (req, res) {
 
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('index', {
-    layout: 'index',
+    layout: 'bootstrap',
 
     heading: {
       name: {
